f9ad3a12c662e14ad7630823c4f01a784346b12c
nativel <nativelbk@gmail.com>
2025-05-17 17:49:22 +0300

    feat: add test

--- api/test.js
+++ api/test.js
@@ -1,43 +1,31 @@
 const { ChatMistralAI } = require("@langchain/mistralai");
 require("dotenv").config();
+const { PromptTemplate } = require("@langchain/core/prompts");
+const { RunnableSequence } = require("@langchain/core/runnables");
+
+const prompt =
+  PromptTemplate.fromTemplate(`devine la langue et donne le reponse  en JSON.
+
+phrase : {phrase}
+
+Réponse (au format JSON, sans texte autour) :
+{{
+  "language": "string"
+}}`);
+
+// Formater le prompt avec une valeur
 
 const chat = new ChatMistralAI({
-  model: "open-mistral-7b", // ou mistral-tiny, mistral-medium (si payant)
+  model: "open-mistral-7b",
   temperature: 0,
   apiKey: process.env.MISTRAL_AI_API_KEY,
 });
 
+const chain = RunnableSequence.from([prompt, chat]);
+
 async function run() {
-  const res = await chat.invoke([
-    { role: "user", content: "Bonjour, comment vas-tu ?" },
-  ]);
+  const res = await chain.invoke({ phrase: "hola" });
   console.log(res);
 }
 
 run();
-
-// const { MistralAI } = require("@langchain/mistralai");
-// const { PromptTemplate } = require("@langchain/core/prompts");
-// // const { LLMChain } = require("@langchain/core/");
-// require("dotenv").config();
-
-// const model = new MistralAI({
-//   model: "mistral-small",
-//   temperature: 0,
-//   apiKey: process.env.MISTRAL_AI_API_KEY,
-// });
-
-// const prompt = new PromptTemplate({
-//   inputVariables: ["input"],
-//   template: "Réponds à ce message : {input}",
-// });
-
-// // const chain = new LLMChain({ llm: model, prompt });
-
-// async function run() {
-//   const response = prompt.pipe(model).invoke({ input: "bonjour" });
-//   //   const response = await chain.call({ input: "bonjour" });
-//   console.log(response.text);
-// }
-
-// run();
