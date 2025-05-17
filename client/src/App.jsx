4f24656f339d1758af5438a91108c66bcb4234e2
nativel <nativelbk@gmail.com>
2025-05-17 22:30:25 +0300

    add changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,21 @@
-import React from 'react'
+import React from "react";
 import LandingPage from "./pages/LandingPage.jsx";
 import CreatePage from "./pages/CreatePage.jsx";
 import ExamplesPage from "./pages/ExamplesPage.jsx";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import HowItWorks from './components/HowItWorks.jsx';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HowItWorks from "./components/HowItWorks.jsx";
 
 function App() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<LandingPage/>}/>
-                <Route path="/create" element={<CreatePage/>}/>
-                <Route path="/examples" element={<ExamplesPage/>}/>
-                <Route path="/how-it-works" element={<HowItWorks/>} />
-            </Routes>
-        </BrowserRouter>
-    )
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/create" element={<CreatePage />} />
+        <Route path="/examples" element={<ExamplesPage />} />
+        <Route path="/how-it-works" element={<HowItWorks />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
